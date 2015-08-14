08cfd2fd1325e305c3e6df6fc3c82fdfd51eab56
Bill Davidson <bill.davidson@gmail.com>
2015-08-14 13:34:29 -0700

    Catch zero length identifiers as well.

--- JSONUtil/src/main/java/org/kopitubruk/util/json/JSONUtil.java
+++ JSONUtil/src/main/java/org/kopitubruk/util/json/JSONUtil.java
@@ -655,7 +655,7 @@ public class JSONUtil
     {
         String propertyName = key == null ? null : key.toString();
 
-        if ( propertyName == null ){
+        if ( propertyName == null || propertyName.length() == 0 ){
             throw new BadPropertyNameException(propertyName, jsonConfig);
         }
 
