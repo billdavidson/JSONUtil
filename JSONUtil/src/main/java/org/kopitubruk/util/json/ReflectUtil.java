c1b27110a0b86cebfa832238f7188c860e5c6208
Bill Davidson <bill.davidson@gmail.com>
2016-08-14 15:00:01 -0700

    Minimal size constructors.

--- JSONUtil/src/main/java/org/kopitubruk/util/json/ReflectUtil.java
+++ JSONUtil/src/main/java/org/kopitubruk/util/json/ReflectUtil.java
@@ -168,7 +168,7 @@ public class ReflectUtil
     private static synchronized Map<Class<?>,Map<Field,Method>> getFieldMethodCompat()
     {
         if ( FIELD_METHOD_COMPAT == null ){
-            FIELD_METHOD_COMPAT = new HashMap<>();
+            FIELD_METHOD_COMPAT = new HashMap<>(0);
         }
         return FIELD_METHOD_COMPAT;
     }
@@ -181,7 +181,7 @@ public class ReflectUtil
     private static synchronized Map<Class<?>,Map<Field,Method>> getFieldMethodIncompat()
     {
         if ( FIELD_METHOD_INCOMPAT == null ){
-            FIELD_METHOD_INCOMPAT = new HashMap<>();
+            FIELD_METHOD_INCOMPAT = new HashMap<>(0);
         }
         return FIELD_METHOD_INCOMPAT;
     }
